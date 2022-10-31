38a9e1f23e8129ccaf8812ef9c37ff8dc95de309
Amin_stors <nurmatkyzysamira@gmail.com>
2022-10-31 18:00:15 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     // tablitsa();
+
+   int[]massiv={1,5,6,8,9,10,45,15,12,};
+   nam(massiv);
+
+
+
+       /* Scanner san=new Scanner(System.in);
+        int a= san.nextInt();
+        if (a==1){
+            Hello();
+        } else if(a==2) {
+            bye();{
+            }
+        }else{
+            System.out.println(" minday medot jok");
+        }
+    }
+    /*public  static  void tablitsa(){
+        for (int i = 2; i <=10 ; i++) {
+            for (int j = 1; j <=10; j++) {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+        System.out.println();
+
+
+     public static void Hello() {
+         System.out.println(" Hello");
+
+     }
+     public static void bye(){
+         System.out.println(" bye");
+
+        */
+
+
+
+     }
+     public static void nam(int[] san){
+             System.out.println(Arrays.toString(san));
+
+         }
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
